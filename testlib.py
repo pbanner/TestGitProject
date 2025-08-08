161ce1fdf959fceb6a71950cc1311bc1d578e3f6
Patrick Banner <53546824+pbanner@users.noreply.github.com>
2025-08-08 14:01:09 -0400

    Testing rebasing 1

--- testlib.py
+++ testlib.py
@@ -28,3 +28,16 @@ def testDiv(a, b):
     if b == 0:
         raise ValueError("Division by zero is not allowed.")
     return a/b
+
+
+def testFib(n):
+    """  Returns the nth Fibonacci number. """
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
